1649eceec0b5a318278d283139cf0465775ed2a7
yibre <cdoyue@gmail.com>
2025-11-28 15:51:55 +0900

    [FEAT] 프로젝트 구조 생성 중

--- core/config.py
+++ core/config.py
@@ -3,17 +3,18 @@ from pydantic_settings import BaseSettings
 
 load_dotenv()
 
+class Settings(BaseSettings):
+    DB_USER: str = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "postgres")
+    DB_HOST: str = os.getenv("DB_HOST", "localhost")
+    DB_PORT: str = os.getenv("DB_PORT", "5432")
+    DB_NAME: str = os.getenv("DB_NAME", "fastapidb")
+    
+    # Construct the database URL
+    DATABASE_URL: str = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-class Config(BaseSettings):
-    app_name: str = "mijixa"
-    debug: bool = False
-    db_user: str = "postgres"
-    db_password: str = ""
-    db_name: str = "mijixa2025.db"
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
 
-    @property
-    def db_url(self):
-        return f"sqlite:///./{self.db_name}"
-
-
-config = Config()
+settings = Settings()
