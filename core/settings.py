56bb4ac04a3cd51547c80ad2f620a584acbfc432
yibre <cdoyue@gmail.com>
2025-12-05 06:27:56 +0900

    [FEAT] 프로젝트 구조 빌딩중

--- core/settings.py
+++ core/settings.py
@@ -4,15 +4,15 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 class Settings(BaseSettings):
-    DB_USER: str = os.getenv("DB_USER", "postgres")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "postgres")
-    DB_HOST: str = os.getenv("DB_HOST", "localhost")
-    DB_PORT: str = os.getenv("DB_PORT", "5432")
-    DB_NAME: str = os.getenv("DB_NAME", "fastapidb")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: str = os.getenv("DB_PORT")
+    DB_NAME: str = os.getenv("DB_NAME")
     
     # Construct the database URL
     DATABASE_URL: str = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
+    
     class Config:
         case_sensitive = True
         env_file = ".env"
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
-import routers.api as api
-import routers.search as search
+# import routers.api as api
+# import routers.search as search
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
@@ -13,11 +13,10 @@ templates = Jinja2Templates(directory="templates")
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # router 추가
-app.include_router(api.router)
-app.include_router(search.router)
+#app.include_router(api.router)
+# app.include_router(search.router)
 
 
 # run server by 'python main.py' in windows
 if __name__ == '__main__':
-    #uvicorn.run('main:app', reload=True)
     uvicorn.run('main:app', host="0.0.0.0", port=80, reload=True)
